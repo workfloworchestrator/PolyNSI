2ae0c554f2200d59fe10792a3ca5e5f1f95dce5d
Guido Kollerie <guido@kollerie.com>
2020-04-03 10:58:00 +0200

    Don't commit Apache CXF generated code.
    * Deleted all generated code.
    * Added paths for generated code to .gitignore.
    * Added paths for generated code to Maven clean target.
    * Updated README.md with instrutions for generating code.
    
    Code can be regenerated by means of:
    
        mvn clean generate-sources

--- src/main/java/nl/surf/polynsi/soap/framework/headers/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.ogf.org/nsi/2013/12/framework/headers")
-package nl.surf.polynsi.soap.framework.headers;
--- src/main/java/nl/surf/polynsi/soap/services/p2p/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.ogf.org/nsi/2013/12/services/point2point")
-package nl.surf.polynsi.soap.services.p2p;
