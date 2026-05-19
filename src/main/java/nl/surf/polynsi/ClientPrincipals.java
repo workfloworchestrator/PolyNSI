b2d7411f2fdd728dd3fc286338c0956cd16b314a
Arno Bakker <arno.bakker@surf.nl>
2026-05-19 14:22:48 +0200

    Claude test fix 4: problematic OIDs

--- src/main/java/nl/surf/polynsi/ClientPrincipals.java
+++ src/main/java/nl/surf/polynsi/ClientPrincipals.java
@@ -1,7 +1,9 @@
 package nl.surf.polynsi;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 import javax.security.auth.x500.X500Principal;
 import org.springframework.stereotype.Component;
@@ -42,10 +44,20 @@ public class ClientPrincipals {
             try {
                 // Arno: this parser is somewhat flexible regarding format. WONTFIX more flexible parsing as in
                 // nsi-auth.
-                // https://docs.oracle.com/en/java/javase/26/docs/api/java.base/javax/security/auth/x500/X500Principal.html#%3Cinit%3E(java.lang.String)
+                // https://docs.oracle.com/en/java/javase/26/docs/api/java.base/javax/security/auth/x500/X500Principal.html#%3Cinit%3E(java.lang.String,java.util.Map)
                 // "The distinguished name must be specified using the grammar defined in RFC 1779 or RFC 2253 (either
                 // format is acceptable)."
-                X500Principal p = new X500Principal(propDistinguishedName);
+                Map<String, String> names2oid = new HashMap<>();
+                // Not all x509 implementations do not know the symbolic names for all OIDs.
+                // This table fixes the most common ones.
+                // Keep synchronized with nsi-auth/rfc4514_cmp.py
+                // "Keywords MUST be specified in all upper-case, otherwise they will be ignored."
+                names2oid.put("SN", "2.5.4.4"); // surname
+                names2oid.put("GN", "2.5.4.42"); // givenname
+                names2oid.put("ORGANIZATIONIDENTIFIER", "2.5.4.97");
+                names2oid.put("EMAILADDRESS", "1.2.840.113549.1.9.1");
+
+                X500Principal p = new X500Principal(propDistinguishedName, names2oid);
                 this.allowedPrincipals.add(p);
             } catch (Exception e) {
                 LOG.fine(propDistinguishedName + " not a proper DN:" + e);
@@ -62,6 +74,9 @@ public class ClientPrincipals {
         for (X500Principal allowedPrincipal : allowedPrincipals) {
             // Main authentication line
             // javax.security.auth.x500.X500Principal object equals() method does comparison
+
+            System.out.println("CMP " + sslClientSubjectPrincipal + " TO " + allowedPrincipal + "#");
+
             if (sslClientSubjectPrincipal.equals(allowedPrincipal)) return true;
         }
         return false;
